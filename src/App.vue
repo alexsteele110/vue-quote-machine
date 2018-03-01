76668243ab952e0654fcf3b7d6d25853b5063e03
Alex Steele <alexsteele110@gmail.com>
2018-03-01 15:28:22 -0500

    functionality to add quotes, info box at bottom added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="container">
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
       <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">
+            Info: Click on a quote to delete it!
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
 
 export default {
   data: function() {
@@ -14,8 +23,14 @@ export default {
       maxQuotes: 10
     };
   },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
+  },
   components: {
-    appQuoteGrid: QuoteGrid
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
   }
 };
 </script>
